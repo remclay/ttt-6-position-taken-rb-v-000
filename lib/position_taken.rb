659421e2a454f2b6fd413f33396091f9bc3776ce
Remmy Clay <remmy.lauren@gmail.com>
2017-11-06 05:50:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,5 @@ def position_taken?(board, index)
   if (board[index] != " " || board[index] != "")
     return false
   elsif (board[index] == "X" || board[index] == "O")
-    return true
   end
 end
